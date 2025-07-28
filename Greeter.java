8e1d844972daf7ba550075662cd0b3fbbed96211
sawsan <oshera@hotmail.com>
2025-07-28 19:45:49 +1200

    Resolved merge conflict between feature-1 and feature-2

--- Greeter.java
+++ Greeter.java
@@ -13,7 +13,7 @@ public class Greeter {
 
   public String sayHello() {
     ResourceBundle messages = ResourceBundle.getBundle("MessagesBundle", locale);
-     return ""Hello, **" + name.toUpperCase + "**!";
+     return "Hello, **" + name.toUpperCase() + "**!";
   }
 
   public static void main(String[] args) {
